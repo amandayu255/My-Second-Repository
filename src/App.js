6699a2e13e822cd407a0045b64aa2fe85711a7e1
amandayu255 <69171765+amandayu255@users.noreply.github.com>
2021-12-15 17:40:39 -0800

    added more people and apps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 
-function displayPerson(first, last, apps) {
+function displayThings(first, last, apps) {
   return (
     <div>
       Hello {first} {last}! You like {apps}.
@@ -8,37 +8,31 @@ function displayPerson(first, last, apps) {
   );
 }
 
-function displayApps(one, two) {
-  return (
-    <div>
-      {one}
-      {two}!
-    </div>
-  );
-}
-
 export default function App() {
   let people = [
-    ["Victor", "Lymar"],
-    ["Traci", "Siebel"]
+    ["Victor", "Lymar", "Instagram"],
+    ["Traci", "Siebel", "Instagram"],
+    ["Ida", "Voong", "Instagram"],
+    ["Ana", "Rivera", "Instagram"],
+    ["Aidan", "Fuller", "Instagram"],
+    ["kyle", "wan", "Ticktok"],
+    ["Enjun", "Li", "Instagram"],
+    ["Luis", "Garcia", "Instagram"],
+    ["Amanda", "Yu", "Instagram"],
+    ["Yifei", "Du", "Instagram"],
+    ["Hector", "Del Valle", "Instagram"],
+    ["HaoYu", "Lin", "Instagram"],
+    []
   ];
 
   let elements = [];
-  for (let person of people) {
-    let first = person[0];
-    let last = person[1];
-    elements.push(displayPerson(first, last));
-  }
-
-  let apps = [["Instagram"], ["Tik Tok"]];
-
-  let media = [];
-  for (let app of apps) {
-    let one = app[0];
-    let two = app[1];
-    elements.push(displayApps(app));
+  for (let i = 0; i < 12; i++) {
+    console.log(people[i]);
+    let first = people[i][0];
+    let last = people[i][1];
+    let app = people[i][2];
+    elements.push(displayThings(first, last, app));
   }
 
   return <div> {elements} </div>;
-  return <div> {media} </div>;
 }
