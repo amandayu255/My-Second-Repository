245b95a42bb7e28857f1889ccce1aa283c0cfb31
amandayu255 <69171765+amandayu255@users.noreply.github.com>
2022-01-19 16:39:13 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import "./styles.css";
 
-function displayThings(first, last, apps, year, color, scene) {
+function displayThings(first, last, apps, year, color) {
   return (
     <div>
       Hello {first} {last}! You like {apps} and the color {color}. You graduate
-      in {year}. You prefer {scene}.
+      in {year}. 
     </div>
   );
 }
 
 export default function App() {
   let people = [
-    ["Victor", "Lymar", "Instagram", 1923, "green", "mountains"],
-    ["Traci", "Siebel", "Instagram", 2022, "purple", "mountains"],
-    ["Ida", "Voong", "Instagram", 2022, "blue", "beach"],
-    ["Ana", "Rivera", "Instagram", 2051, "green", "beach"],
-    ["Aidan", "Fuller", "Instagram", 2023, "red", "beach"],
-    ["kyle", "wan", "Ticktok", 2023, "yeah", "mountains"],
-    ["Enjun", "Li", "Instagram", 2022, "black", "mountains"],
-    ["Luis", "Garcia", "Instagram", 2025, "black", "beach"],
-    ["Amanda", "Yu", "Instagram", 2022, "blue", "mountains"],
-    ["Yifei", "Du", "Instagram", 2022, "black", "beach"],
-    ["Hector", "Del Valle", "Instagram", 2022, "gray", "beach"],
-    ["HaoYu", "Lin", "Instagram", 2023, "red", "beach"]
+    ["Victor", "Lymar", "Instagram", 1923, "green"],
+    ["Traci", "Siebel", "Instagram", 2022, "purple"],
+    ["Ida", "Voong", "Instagram", 2022, "blue"],
+    ["Ana", "Rivera", "Instagram", 2051, "green"],
+    ["Aidan", "Fuller", "Instagram", 2023, "red"],
+    ["kyle", "wan", "Ticktok", 2023, "yeah"],
+    ["Enjun", "Li", "Instagram", 2022, "black"],
+    ["Luis", "Garcia", "Instagram", 2025, "black"],
+    ["Amanda", "Yu", "Instagram", 2022, "blue"],
+    ["Yifei", "Du", "Instagram", 2022, "black"],
+    ["Hector", "Del Valle", "Instagram", 2022, "gray"],
+    ["HaoYu", "Lin", "Instagram", 2023, "red"]
   ];
 
   let elements = [];
@@ -33,8 +33,7 @@ export default function App() {
     let app = people[i][2];
     let year = people[i][3];
     let color = people[i][4];
-    let scene = people[i][5];
-    elements.push(displayThings(first, last, app, year, color, scene));
+    elements.push(displayThings(first, last, app, year, color));
   }
 
   return <div> {elements} </div>;
